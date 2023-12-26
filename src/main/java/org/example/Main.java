294235117a2ffa354170cfe991a91e998493e861
GrezizX <145987331+GrezizX@users.noreply.github.com>
2023-12-26 21:05:17 +0500

    Update Main.java
    Avazdan Salom

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Java");
+        System.out.printf("Java N7 Group");
 
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {

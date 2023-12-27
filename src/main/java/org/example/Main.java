23646e4ae2e5eb15da1b3ee5670ebc9412ae8c84
Saidafzalkhon <Saidafzalkhon.developer@gmail.com>
2023-12-27 20:56:36 +0500

    B

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,7 +12,7 @@ public class Main {
         for (int i = 1; i <= 5; i++) {
             // Press Shift+F9 to start debugging your code. We have set one breakpoint
             // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = i" + i);
+            System.out.println("i " + i);
         }
         System.out.println("Ganiev Mirzabek");
     }
